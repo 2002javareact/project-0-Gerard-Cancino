8cc0dbc848da59946b5983a872b2458f566e8f02
Gerard Cancino <gcancino@pop-os.localdomain>
2020-03-05 02:54:46 -0500

    Debug Server to DB

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {securityMiddleware} from './middleware/security-middleware';
 const app = express();
 
 app.use('/',(req,res,next)=>{
+  console.log(process.env.REVATURE_HOST)
   console.log('connecting to link');
   next();
 })
